3e7f66007927e4bc692ea71d5dd7163dcf51e4ee
mankcom3799@hufs.ac.kr <mankcom3799@hufs.ac.krecho>
2023-12-12 20:31:21 +0900

    init commit

---
+++ src/test/java/com/pjh/refreshtoken/RefreshTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjh.refreshtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefreshTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
